038de8bedbaf0648ded80dee118e232ff770b6a1
ctaylor <ctaylor@localhost>
2022-01-18 14:02:10 -0500

    Initial Project commit

---
+++ DataStructures_Unit1/src/TestProgram.java
@@ -0,0 +1,12 @@
+
+public class TestProgram 
+{
+	public static void main(String [] args)
+	{
+		System.out.println("""
+				Hello my name is Cymantha Blackmon
+				I am a Computational Science and Engineering STEM Major
+				:)
+				""");
+	}
+}
